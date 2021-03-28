68403ed08784e429eaaacca55bc5c2edff822e23
roromaniac <roko@umich.edu>
2021-03-27 22:45:18 -0500

    pylint customization

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 """This is just so that pylint will stop fucking yelling at me."""
 # import torch
+# import torchvision
 # from torch.utils.tensorboard import SummaryWriter
+
 
 # x = 12345
 print("Test commit")
