5c34ac8ac865346b19be3d158967d93bcae83e7b
roromaniac <roko@umich.edu>
2021-03-27 22:47:52 -0500

    newest stuff

--- model.py
+++ model.py
@@ -1,9 +1,7 @@
 """This is just so that pylint will stop fucking yelling at me."""
-import torch
+# import torch
 # import torchvision
 # from torch.utils.tensorboard import SummaryWriter
-print(torch.utils)
-x = torch.utils
 
 # x = 12345
 print("Test commit")
