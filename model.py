95c697983afaace4a2ca01c4123b214bc9e89383
roromaniac <roko@umich.edu>
2021-03-27 22:39:06 -0500

    pylint test #10

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
-import torch
-from torch.utils.tensorboard import SummaryWriter
+"""This is just so that pylint will stop fucking yelling at me."""
+# import torch
+# from torch.utils.tensorboard import SummaryWriter
 
-x = 1234
+# x = 12345
+print("Test commit")
