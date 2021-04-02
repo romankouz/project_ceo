14c57b6605b3f473a4e897b23fcda48795dd0624
roromaniac <roko@umich.edu>
2021-04-02 17:53:00 -0500

    CIFAR implementation

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ import torch.optim
 import pytorch_lightning as pl
 from pytorch_lightning.metrics.functional import accuracy
 
+
 class MNISTPredictor(pl.LightningModule):
 
     """This is just so that pylint will stop fucking yelling at me."""
@@ -34,6 +35,7 @@ class MNISTPredictor(pl.LightningModule):
 
     def training_step(self, train_batch, batch_idx):
         inputs, target = train_batch
+        inputs = inputs.mean(1)
         inputs = inputs.view(inputs.size(0), -1)
         out = self(inputs)
         loss = F.cross_entropy(out, target)
@@ -45,6 +47,56 @@ class MNISTPredictor(pl.LightningModule):
 
     def validation_step(self, val_batch, batch_idx):
         inputs, target = val_batch
+        inputs = inputs.mean(1)
+        inputs = inputs.view(inputs.size(0), -1)
+        out = self(inputs)
+        loss = F.cross_entropy(out, target)
+        preds = torch.argmax(out, dim=1)
+        acc = accuracy(preds, target)
+        self.log('val_loss', loss)
+        self.log('val_acc', acc)
+        return loss
+
+
+class CIFAR10Predictor(pl.LightningModule):
+
+    """This is just so that pylint will stop fucking yelling at me."""
+
+    def __init__(self, optim="Adam"):
+        super().__init__()
+        self.optim = optim
+        self.forward_pass = nn.Sequential(
+            nn.Linear(32*32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 10)
+        )
+
+    def forward(self, x):
+        return self.forward_pass(x)
+
+    def configure_optimizers(self):
+        optimizer = getattr(torch.optim, self.optim)(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        inputs, target = train_batch
+        inputs = torch.mean(inputs, 1)
+        inputs = inputs.view(inputs.size(0), -1)
+        out = self(inputs)
+        loss = F.cross_entropy(out, target)
+        preds = torch.argmax(out, dim=1)
+        acc = accuracy(preds, target)
+        self.log('train_loss', loss)
+        self.log('train_acc', acc)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        inputs, target = val_batch
+        inputs = torch.mean(inputs, 1)
         inputs = inputs.view(inputs.size(0), -1)
         out = self(inputs)
         loss = F.cross_entropy(out, target)
--- train.py
+++ train.py
@@ -5,23 +5,30 @@ import argparse
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-from torchvision.datasets import MNIST
+import torchvision.datasets
 from torchvision import transforms
-from model import MNISTPredictor
+from model import MNISTPredictor, CIFAR10Predictor
 
 def train(dataset="MNIST", optim="Adam"):
     """This is just so that pylint will stop fucking yelling at me."""
-    if dataset == "MNIST":
-        dataset = MNIST('', train=True, download=os.path.isdir('MNIST'),
-                            transform=transforms.ToTensor())
-        mnist_train, mnist_val = random_split(dataset, [55000, 5000])
-        train_loader = DataLoader(mnist_train, batch_size=32)
-        val_loader = DataLoader(mnist_val, batch_size=32)
-        # model
+    data = getattr(torchvision.datasets, dataset)('./', train=True, 
+                        download=not os.path.isdir(dataset),
+                        transform=transforms.ToTensor())
+    n_data = len(data)
+    train_n = round(11*n/12)
+    train_data, val_data = random_split(data, [train_n, n_data-train_n])
+    # model
+    if dataset=="MNIST":
         model = MNISTPredictor(optim)
-        # training
-        trainer = pl.Trainer(gpus=1, precision=16, max_epochs=500)
-        trainer.fit(model, train_loader, val_loader)
+        train_loader = DataLoader(train_data, batch_size=32)
+        val_loader = DataLoader(val_data, batch_size=32)
+    if dataset=="CIFAR10":
+        model = CIFAR10Predictor(optim)
+        train_loader = DataLoader(train_data, batch_size=32)
+        val_loader = DataLoader(val_data, batch_size=32)
+    # training
+    trainer = pl.Trainer(gpus=1, precision=16, max_epochs=2)
+    trainer.fit(model, train_loader, val_loader)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -35,6 +42,6 @@ if __name__ == '__main__':
         train(args.dataset if args.dataset is not None else "MNIST", "RMSprop")
         train(args.dataset if args.dataset is not None else "MNIST", "SGD")
     else:
-        train(args.dataset if args.dataset is not None else "MNIST",
+        train(args.dataset if args.dataset is not None else "CIFAR10",
               args.optim if args.optim is not None else "Adam")
         
